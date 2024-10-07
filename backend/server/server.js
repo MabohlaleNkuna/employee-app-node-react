851be2422ffd8022b2ffa3b78544175c571edb1c
MabohlaleNkuna <111710433+MabohlaleNkuna@users.noreply.github.com>
2024-10-07 16:02:52 +0200

    upd

--- backend/server/server.js
+++ backend/server/server.js
@@ -5,9 +5,11 @@ const { db, bucket } = require('./firebaseAdmin');
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
 
-// Add an employee
+
+app.use(bodyParser.json({ limit: '10mb' })); 
+
+
 app.post('/employees', async (req, res) => {
   const { name, surname, email, idNum, position, department, phone, startDate, imageUrl } = req.body;
 
@@ -29,22 +31,16 @@ app.post('/employees', async (req, res) => {
   }
 });
 
-// Fetch all employees
 app.get('/employees', async (req, res) => {
-  
   try {
-    
     const employeesSnapshot = await db.collection('employees').get();
     const employees = employeesSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    
-
     res.status(200).send(employees);
   } catch (error) {
     res.status(500).send({ message: 'Error fetching employees', error });
   }
 });
 
-// Update an employee
 app.put('/employees/:id', async (req, res) => {
   const { id } = req.params;
   const employeeData = req.body;
@@ -57,7 +53,7 @@ app.put('/employees/:id', async (req, res) => {
   }
 });
 
-// Delete an employee
+
 app.delete('/employees/:id', async (req, res) => {
   const { id } = req.params;
 
@@ -69,7 +65,6 @@ app.delete('/employees/:id', async (req, res) => {
   }
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
